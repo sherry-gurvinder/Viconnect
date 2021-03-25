363091cb47034e4b5999fc7afa4f86ccd1f59728
jagdeepkaur443 <jass28081992@gmail.com>
2021-03-25 17:05:51 -0400

    some modification and creation of about us

--- script/news.js
+++ script/news.js
@@ -1,7 +1,7 @@
 const key = "50a2ce5640bb4470975dac3abda416d7"; //Apikey generated from news api
 const url = `https://newsapi.org/v2/top-headlines?country=ca&category=business&apiKey=${key}`;
 
-const recievedNews = (newsdata) => {
+const receivedNews = (newsdata) => {
   const articlesDiv = document.querySelector(".articles");
   //looping through all news
   newsdata.articles.forEach((article) => {
@@ -9,13 +9,13 @@ const recievedNews = (newsdata) => {
     const div = document.createElement("div");
     div.className = "news";
     div.innerHTML = `
-			<h2>${article.title}</h2>
-			<img src="${article.urlToImage}"/>`;
+          <h2>${article.title}</h2>
+          <img src="${article.urlToImage}"/>`;
     articlesDiv.appendChild(div);
   });
 };
 
-//sending request to api and js promise running it
+// //sending request to api and js promise running it
 fetch(url)
   .then((response) => response.json())
-  .then(recievedNews);
+  .then(receivedNews);
