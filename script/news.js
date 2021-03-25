79cfe4ee39bb1790ac63165f26e410c95c9e6332
jagdeepkaur443 <jass28081992@gmail.com>
2021-03-25 10:42:22 -0400

    script file for news

---
+++ script/news.js
@@ -0,0 +1,21 @@
+const key = "50a2ce5640bb4470975dac3abda416d7"; //Apikey generated from news api
+const url = `https://newsapi.org/v2/top-headlines?country=ca&category=business&apiKey=${key}`;
+
+const recievedNews = (newsdata) => {
+  const articlesDiv = document.querySelector(".articles");
+  //looping through all news
+  newsdata.articles.forEach((article) => {
+    //creating divs and then add to our news html file
+    const div = document.createElement("div");
+    div.className = "news";
+    div.innerHTML = `
+			<h2>${article.title}</h2>
+			<img src="${article.urlToImage}"/>`;
+    articlesDiv.appendChild(div);
+  });
+};
+
+//sending request to api and js promise running it
+fetch(url)
+  .then((response) => response.json())
+  .then(recievedNews);
