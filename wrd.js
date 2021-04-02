625660b08f9d5499965b61e3c5654bf49e44396d
sherry-gurvinder <sgurvinder0594@gmail.com>
2021-04-01 20:57:40 -0400

    Signup and login functionality done by local storage

--- wrd.js
+++ wrd.js
@@ -1,43 +1,110 @@
 
 function validate()
 {
-    var fname = document.getElementById("fname");
-    var lname = document.getElementById("lname");
+   alert("hello");
+   debugger
+    var email = $("#email").val();
+    var pass = $("#password").val();
     flag = 0;
    
     
     
    
-    if(fname.value == "")
+    if(email.value == "")
     {
-        document.getElementById("fname-msg").innerHTML = "User Name is required";
-        fname.style.border = " solid #f39c12";
+        document.getElementById("email-msg").innerHTML = "Email is required";
+        email.style.border = " solid #f39c12";
         flag = 0;
     }
     else
-    {
-        document.getElementById("fname-msg").innerHTML = "";
-        flag = 1;
+    {   
+        if(IsEMAILVALID())
+        {
+            document.getElementById("email-msg").innerHTML = "";
+            flag = 1;
+        } 
         
-    }
+        else
+        {
+            document.getElementById("email-msg").innerHTML = "Invalid Email Address";
+            //$("#email").style.border = " solid #f39c12";
+            flag = 0;
+            return false;
+        }
+        
+        
+    }  
 
-    if(lname.value == "")
+    if(pass.value == "")
     {
-        document.getElementById("lname-msg").innerHTML = "Password is required";
-        lname.style.border = " solid #f39c12";
+        document.getElementById("password-msg").innerHTML = "Password is required";
+        document.getElementById("password").style.border = " solid #f39c12";
         flag = 0;
     }
     else
     {
-        document.getElementById("lname-msg").innerHTML = "";
+        document.getElementById("password-msg").innerHTML = "";
         flag = 1;
     }
+    debugger
     if (flag == 0)
     {
         return false;
     }
+    else
+    {
+        return getStore();
+    }
+
+    
 
    
 } ;
 
+function getStore(){
+	
+    var inputEmail= $("#email").val();
+    var inputPassword= $("#password").val();
+   debugger;
+  var retrievedObject = localStorage.getItem(inputEmail);
+  var obj = JSON.parse(retrievedObject);
+  alert(obj);
+  if(obj!=null)
+    {
+    if(obj.email == inputEmail && obj.password == inputPassword)
+    { 
+        alert("Logged IN")
+        return true;
+    }
+    else
+    {   
+        document.getElementById("email-msg").innerHTML = "Wrong credentials";
+        alert("Wrong credentials");
+        return false;
+    }
+    }
+    else
+    {
+        document.getElementById("email-msg").innerHTML = "Wrong credentials";
+        alert("Wrong credentials");
+        return false;
+    }
+}
 
+
+
+  function IsEMAILVALID() {
+
+
+    var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+
+    if (pattern.test($("#email").val())) {
+        return true;
+        
+       
+    } else {
+       
+        return false;
+    }
+
+}
