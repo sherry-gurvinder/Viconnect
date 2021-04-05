c385380a53c3fe9b2da7e7e3ba01b48c09f0ea45
jagdeepkaur443 <jass28081992@gmail.com>
2021-04-04 23:22:48 -0400

    api provider changed and responsive design for news page

--- script/news.js
+++ script/news.js
@@ -1,32 +1,26 @@
-// key = "50a2ce5640bb4470975dac3abda416d7"; //Apikey generated from news api
-// const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-const url = `https://newsapi.org/v2/top-headlines?country=ca&category=business&apiKey=50a2ce5640bb4470975dac3abda416d7`;
-
 const receivedNews = (newsdata) => {
   const articlesDiv = document.querySelector(".articles");
   //looping through all news
-  // newsdata.articles.forEach((article) => {
-  //   //creating divs and then add to our news html file
-  //   const div = document.createElement("div");
-  //   div.className = "news";
-  //   div.innerHTML = `
-  //         <h2>${article.title}</h2>
-  //         <img src="${article.urlToImage}"/>`;
-  //   articlesDiv.appendChild(div);
-  // });
-
     for(var i =0; i<=newsdata.articles.length; i++){
       //creating divs and then add to our news html file
       const div = document.createElement("div");
       div.className = "news";
       div.innerHTML = `
             <h2>${newsdata.articles[i].title}</h2>
-            <img src="${newsdata.articles[i].urlToImage}"/>`;
+            <img src="${newsdata.articles[i].media}"/>
+            <p>${newsdata.articles[i].summary}</p>
+            <hr>`;
       articlesDiv.appendChild(div);
   };
 };
 
 // //sending request to api and js promise running it
-fetch(url)
+fetch("https://newscatcher.p.rapidapi.com/v1/latest_headlines?lang=en&media=True", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "0ab4ae333bmsh76356bcf87f0d7bp1c4fdcjsnf08afa11ea71",
+		"x-rapidapi-host": "newscatcher.p.rapidapi.com"
+	}
+})
   .then((response) => response.json())
   .then(receivedNews);
