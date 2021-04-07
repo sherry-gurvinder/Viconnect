89ea9d4cdd7a4ce34aa5284f540297eef868b9aa
sherry-gurvinder <sgurvinder0594@gmail.com>
2021-04-07 18:15:23 -0400

    Send email UI

--- script/user.js
+++ script/user.js
@@ -13,12 +13,65 @@ var settings = {
         console.log(data['name']);
 
        
-        $(".wrapper").prepend("<div class='box'>"+data['name']+'<p>'+data['email']+'</p> </div>');
+        $(".wrapper").prepend("<div class='box'>"+data['name']+'<p><input class="email" type="email" value='+data['email']+'></p>'+
+        '<input type="button" class="sendemail" value="Send Email"  />'+ '</div>');
         
     });
   
 });
+
+
+
+$('.sendemail').click(function()
+{
+
+  alert("sendemail");
   
+  var email = document.getElementsByClassName("email");
+  alert(email.value);
+  if(email.value == "")
+
+    {
+      debugger
+        document.getElementsByClassName("email").innerHTML = "Email is required";
+        email.style.border = " solid #f39c12";
+        flag = 0;
+    }
+    else
+    {
+        if(IsEMAILVALID())
+        {
+            document.getElementsByClassName("email").innerHTML = "";
+            flag = 1;
+        } 
+        else
+        {
+            document.getElementsByClassName("email").innerHTML = "Invalid Email Address";
+            
+            flag = 0;
+        }
+        
+        
+    }  
+
+
+})
+
+function IsEMAILVALID() {
+  alert("email.com")
+
+  var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+
+  if (pattern.test($("#email").val())) {
+      return true;
+      
+     
+  } else {
+     
+      return false;
+  }
+
+}
   
   
   
